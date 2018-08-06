c84d8779351f7dee73b6ddaf0937b784bc84b397
shamaton <shamaton77@gmail.com>
2018-08-06 13:22:42 +0900

    watch at msgpack

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/shamaton/sandboxgo/msgpack"
+
+func main() {
+	msgpack.F()
+}
---
+++ msgpack/msgpack.go
@@ -0,0 +1,69 @@
+package msgpack
+
+import (
+	"bytes"
+	"encoding/hex"
+	"fmt"
+	"math"
+
+	"github.com/vmihailenco/msgpack"
+)
+
+// F is func
+func F() {
+	p("int 1 -------------->")
+	msg(9)
+	p("int 2 -------------->")
+	msg(127)
+	p("int max -------------->")
+	msg(math.MaxInt32)
+	p("array -------------->")
+	msg([]int{1, 2, 3})
+	p("array 2 -------------->")
+	msg([]int{1, 2, 3, math.MinInt64})
+	p("map -------------->")
+	msg(map[int]int{1: 2, 3: 4, 5: math.MaxInt32})
+	p("string -------------->")
+	msg("0123456789012345678901234567890")
+	p("string 2-------------->")
+	msg("01234567890123456789012345678901")
+
+	p("st 1 -------------->")
+	type st1 struct {
+		A int
+	}
+	msg(st1{A: 7})
+	p("st 2-------------->")
+	type st2 struct {
+		A int
+		B string
+	}
+	msg(st2{A: 7, B: "7"})
+	p("-------------->")
+	// p("-------------->")
+
+}
+
+func msg(v ...interface{}) {
+	b, e := msgpack.Marshal(v...)
+	printDump("def", b, e)
+
+	var buf bytes.Buffer
+	enc := msgpack.NewEncoder(&buf).StructAsArray(true)
+	err := enc.Encode(v...)
+	printDump("arr", buf.Bytes(), err)
+}
+
+func printDump(t string, b []byte, e error) {
+	if e != nil {
+		fmt.Println("error : ", e)
+		return
+	}
+	fmt.Println(t, ":")
+	fmt.Println(hex.Dump(b))
+	fmt.Println("")
+}
+
+func p(v ...interface{}) {
+	fmt.Println(v...)
+}
